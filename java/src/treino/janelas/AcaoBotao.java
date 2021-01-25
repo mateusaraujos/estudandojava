4c61932642665a4a2655d5a141761c32b6d5c6bd
Mateus Araújo <silva.mateusaraujo98@gmail.com>
2021-01-25 01:51:06 -0300

    Evoluindo

---
+++ java/src/treino/janelas/AcaoBotao.java
@@ -0,0 +1,41 @@
+package treino.janelas;
+
+// Um Listener de eventos de botão deve implementar a interface ActionListener.
+import java.awt.event.ActionListener;
+
+// Sou obrigado a implementar actionPerformed() com a classe ActionEvent.
+import java.awt.event.ActionEvent;
+
+// Para uma referência ao componete da caixa de texto que criamos.
+import javax.swing.JTextField;
+
+// Para abrir uma caixa de diálogo.
+import javax.swing.JOptionPane;
+
+
+// Linha 50 de Janela.java - Temos que criar o Listener.
+
+public class AcaoBotao implements ActionListener {
+
+    // Referência ao componente da caixa de texto.
+    private JTextField t;
+
+    // Depois da referência, eu crio um construtor que recebe o t como parâmetro.
+    public AcaoBotao(JTextField t) {
+        this.t = t;
+    }
+
+    // Quando eu implemento a interface ActionListener, sou obrigado a implementar o método actionPerformed().
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // O próprio swing vai invocar esse método.
+        // Com o ActionEvent temos acesso ao componente que disparou o evento.
+        // 0000000000 Antes de tudo, temos que REGISTRAR O LISTENER no botão que criamos para avisar ao java quem está ouvidno o evento do botão.
+
+        // Depois de ter uma referência na classe do TextField da caixa de texto criada.
+        String nome = t.getText();
+        JOptionPane.showMessageDialog(null, "Olá, " + nome + "! Bom te ver por aqui.", "Boas Vindas", 1);
+    }
+
+    
+}
--- java/src/treino/janelas/Janela.java
+++ java/src/treino/janelas/Janela.java
@@ -4,20 +4,59 @@ package treino.janelas;
 
 // A janela é representada por um objeto da classe JFrame.
 import javax.swing.JFrame;
+// ***JPanel para agrupar todos os componentes que eu criar.***
+// É um componente invisível, não enxergamos se não tiver bordas.
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
 public class Janela {
 
     public static void main(String[] args) {
 
         JFrame f = new JFrame();
+
         f.setTitle("Primeira Janela"); // AO LADO - Título da janela.
+
         f.setSize(300, 200); // AO LADO - Tamanho da janela em px (largura, altura).
+
         // ABAIXO - Posição na tela onde a janela será desenhada (contando do lado superior esquerdo do monitor).
         f.setLocation(500, 300);
+
         // ABAIXO - Como o java deve tratar o fechamento da janela. Por padrão, o java só esconde a janela, não fecha.
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         // ACIMA - Fazer a aplicação terminar quando eu fechar a janela com a constante EXIT_ON_CLOSE.
-        // ABAIXO - Para a janela realmente ser desenhada na tela, temos que dizer que ela é visível.
+
+        // ABAIXO - Antes de deixar a janela visível.
+        // ADICIONO O PAINEL NA JANELA, E DEPOIS ADICIONO OS COMPONENTES DENTRO DO PAINEL.
+        JPanel p = new JPanel();
+
+        // ABAIXO - Começando a adicionar componentes ao Panel.
+        // ABAIXO - Escrevendo um texto qualquer na janela.
+        JLabel l = new JLabel("Digite seu nome:");
+        p.add(l); // AO LADO - Adicionei o componente que acabei de fazer ao Panel.
+
+        // ABAIXO - Criando uma caixa de texto para escrever o nome.
+        JTextField t = new JTextField(10); // 10 é o número de colunas.
+        p.add(t);
+
+        // ABAIXO - Criando um botão.
+        JButton b = new JButton("Ok");
+        AcaoBotao acao = new AcaoBotao(t); // AO LADO - Adicionei a classe em que está o LISTENER ++ Com uma referência a ***t*** após criar o showMessegeDialog.
+        b.addActionListener(acao); // AO LADO - Adicionei o objeto acima, como sendo um LISTENER do botão.
+        p.add(b);
+        // 0000000000 Registrando o LISTENER nesse botão ACIMA.
+
+        // TRATAMENTO DE EVENTOS - A ARQUITETURA DE SWING é baseada em ***Listeners***.
+        // Listener - Objetos que ficam "ouvindo" a ocorrencia de eventos.
+        // Por Exemplo: Eu PRECISO de um OBJETO LISTENER que fique "ouvindo" eventos de click no botão.
+
+
+
+
+        f.add(p); // AO LADO - Adicionei o Panel na minha janela/frame.
+        // ABAIXO - Por fim, para a janela realmente ser desenhada na tela, temos que dizer que ela é visível.
         f.setVisible(true);
     }
 }
