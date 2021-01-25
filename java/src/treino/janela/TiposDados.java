6058e225dc6be14c50d168bca1d68bfb0a573812
Mateus Araújo <silva.mateusaraujo98@gmail.com>
2021-01-24 23:49:19 -0300

    Adicionei uma janelinha

---
+++ java/src/treino/janela/TiposDados.java
@@ -0,0 +1,11 @@
+package treino.janela;
+import javax.swing.JOptionPane;
+// Criando interface gráfica
+
+public class TiposDados {
+    public static void main(String[] args) {
+        String nome = "Mateus";
+
+        JOptionPane.showMessageDialog(null, "Meu nome é " + nome + ".\nIsso é só o começo!!!", "Primeira janela", 2);
+    }
+}
