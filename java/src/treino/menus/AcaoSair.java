e2404efd52ea56890f318570295eaa59ecb276ef
Mateus Araújo <silva.mateusaraujo98@gmail.com>
2021-01-25 20:17:22 -0300

    Trabalhando com menus

---
+++ java/src/treino/menus/AcaoSair.java
@@ -0,0 +1,16 @@
+package treino.menus;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+
+public class AcaoSair extends AbstractAction {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.exit(0);
+    }
+
+    
+    
+}
---
+++ java/src/treino/menus/Janela.java
@@ -0,0 +1,47 @@
+package treino.menus;
+
+import javax.swing.*;
+
+public class Janela extends JFrame {
+
+    public Janela() {
+        setTitle("Minha Aplicação");
+        setSize(600, 300);
+        setLocation(400, 200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JMenuBar menu = new JMenuBar();
+
+        JMenu menuArquivo = new JMenu("Arquivo");
+        menuArquivo.setMnemonic('A');
+
+        JMenu menuEditar = new JMenu("Editar");
+        menuEditar.setMnemonic('E');
+
+        JMenuItem itemNovo = new JMenuItem("Novo");
+        itemNovo.setMnemonic('N');
+
+        JMenuItem itemSair = new JMenuItem(new AcaoSair());
+        itemSair.setText("Sair");
+        itemSair.setMnemonic('S');
+
+        menuArquivo.add(itemNovo);
+        menuArquivo.add(itemSair);
+
+        JMenuItem itemCopiar = new JMenuItem("Copiar");
+        itemCopiar.setMnemonic('C');
+
+        menuEditar.add(itemCopiar);
+
+        menu.add(menuArquivo);
+        menu.add(menuEditar);
+
+        setJMenuBar(menu);
+        setVisible(true);
+    }
+    
+    public static void main(String[] args) {
+        new Janela();
+    }
+
+}
