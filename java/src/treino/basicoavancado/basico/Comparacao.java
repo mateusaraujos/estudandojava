d75e11fd6be19fab35d4e99cc128bc4ced46d8dc
Mateus Araújo <silva.mateusaraujo98@gmail.com>
2021-01-27 13:53:08 -0300

    Adicionei Comparacao.java

---
+++ java/src/treino/basicoavancado/basico/Comparacao.java
@@ -0,0 +1,14 @@
+package treino.basicoavancado.basico;
+
+public class Comparacao {
+
+    public static void main(String[] args) {
+
+        /**
+         * ==, !=, >, >=, <, <=
+         */
+        boolean igual = 10 == 10;
+        System.out.println("Igualdade " + igual);
+    }
+    
+}
