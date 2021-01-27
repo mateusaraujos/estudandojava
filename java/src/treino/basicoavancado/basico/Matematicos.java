1aed35afd48023ffabe67cf286a63e0cd4b6f148
Mateus Araújo <silva.mateusaraujo98@gmail.com>
2021-01-27 13:40:48 -0300

    Adicionei Matematicos.java

---
+++ java/src/treino/basicoavancado/basico/Matematicos.java
@@ -0,0 +1,11 @@
+package treino.basicoavancado.basico;
+
+public class Matematicos {
+
+    public static void main(String[] args) {
+
+        float resultado = ((10 + 3 + ((8 * 4) / 10)) / 2);
+        System.out.println(resultado);
+    }
+    
+}
