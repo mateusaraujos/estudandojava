bc0536f498419dde978745d0b97d86e1a2524198
Mateus Araújo <silva.mateusaraujo98@gmail.com>
2021-01-27 13:59:06 -0300

    Adicionei Incremento

---
+++ java/src/treino/basicoavancado/basico/Incremento.java
@@ -0,0 +1,15 @@
+package treino.basicoavancado.basico;
+
+public class Incremento {
+
+    public static void main(String[] args) {
+
+        int numero = 1;
+        ++numero; // Incrementa primeiro.
+        numero++; // Só será increntada na próxima linha.
+        --numero;
+        numero--;
+        System.out.println(numero);
+    }
+    
+}
