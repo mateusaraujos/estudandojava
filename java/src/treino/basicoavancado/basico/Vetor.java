cf1fe44d67dfbfd80db5f7b0aefdf132060d37cf
Mateus Araújo <silva.mateusaraujo98@gmail.com>
2021-01-27 14:27:28 -0300

    Adicionei Vetor.java

---
+++ java/src/treino/basicoavancado/basico/Vetor.java
@@ -0,0 +1,18 @@
+package treino.basicoavancado.basico;
+
+public class Vetor {
+
+    public static void main(String[] args) {
+
+        String[] nomes = new String[3]; // 0, 1 e 3
+        nomes[0] = "Mateus";
+        nomes[1] = "Milene";
+        nomes[2] = "Gabriela";
+
+        System.out.println(nomes[2]);
+
+        String[] nome = {"Mateus", "João Paulo"};
+        System.out.println(nome[1]);
+    }
+    
+}
