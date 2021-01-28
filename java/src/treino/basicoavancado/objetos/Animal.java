660e1781c64d47fc62306900bf4aeb1d784ce587
Mateus Araújo <silva.mateusaraujo98@gmail.com>
2021-01-28 17:02:45 -0300

    Referenciando a superclass
    super( ); dentro dos contrutores
    É a referência a super classe Object que já vem nos contrutores

---
+++ java/src/treino/basicoavancado/objetos/Animal.java
@@ -0,0 +1,34 @@
+package treino.basicoavancado.objetos;
+
+public class Animal {
+
+    /* Padrão
+    public Animal() {
+        super();
+    }
+    */
+
+    String raca;
+    double peso;
+    public Animal(String raca, double peso) {
+        // super(); já é escrito aqui, por causa da classe pai Object
+        super(); // Serve para referenciar a super classe.
+        this.raca = raca;
+        this.peso = peso;
+    }
+
+    /**
+     * SE A CLASSE PAI TIVER UM CONSTRUTOR COM PARÂMETROS, TENHO QUE CHAMAR O CONSTRUTOR COM OS PARÂMETROS.
+     * public  class Cachorro extends Animal {
+     *     public Cachorro() {
+     *         super("pitbull", 30);
+     *     }
+     * }
+     * 
+     */
+
+    public static void main(String[] args) {
+        Animal teste = new Animal();
+    }
+    
+}
