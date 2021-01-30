64545cede7258350659f7f115f9aa09010eb8476
Mateus Araújo <silva.mateusaraujo98@gmail.com>
2021-01-30 14:42:49 -0300

    Imagens
    ImageIcon
    getClass( ).getResource("")

---
+++ java/src/treino/basicoavancado/InterfaceGrafica/Imagem.java
@@ -0,0 +1,27 @@
+package treino.basicoavancado.InterfaceGrafica;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class Imagem extends JFrame {
+
+    ImageIcon imagem = new ImageIcon(getClass().getResource("BorderLayout.png"));
+    // Para adicionar a imagem, coloco ela em uma JLabel
+    JLabel label = new JLabel(imagem);
+
+    public Imagem() {
+        add(label);
+
+        setTitle("Imagens");
+        setSize(600, 400);
+        setLocationRelativeTo(null);
+        setResizable(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Imagem();
+    }
+    
+}
