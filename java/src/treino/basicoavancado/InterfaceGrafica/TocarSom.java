24fefd7fcaf31ea11b35e6911b7e6fdd1d6f972d
Mateus Araújo <silva.mateusaraujo98@gmail.com>
2021-02-02 10:56:09 -0300

    Conseguindo executar áudio
    import java.io.File;
    import javax.sound.sampled.AudioInputStream;
    import javax.sound.sampled.AudioSystem;
    import javax.sound.sampled.Clip;

--- java/src/treino/basicoavancado/InterfaceGrafica/TocarSom.java
+++ java/src/treino/basicoavancado/InterfaceGrafica/TocarSom.java
@@ -4,11 +4,19 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
+import java.io.File;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
 // import java.applet.Applet; Depreciados
 // import java.applet.AudioClip; Depreciados
 // import java.net.URL;
 
 public class TocarSom extends JFrame {
+
+    String caminhoMusica = "D:/Usuários/Mateus/Documentos/Projetos/estudandojava/java/src/treino/basicoavancado/InterfaceGrafica/cheer2.wav";
+    String caminhoMp3 = "D:/Usuários/Mateus/Documentos/Projetos/estudandojava/java/src/treino/basicoavancado/InterfaceGrafica/latido.mp3";
 
     JButton tocar = new JButton("Alegria");
     Font fonte = new Font("Arial", Font.BOLD, 30);
@@ -18,7 +26,7 @@ public class TocarSom extends JFrame {
         tocar.setBackground(Color.WHITE);
         tocar.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                play("cheer2.wav");
+                tocarMusicas(caminhoMusica);
             }
         });
         add(tocar).setFont(fonte);
@@ -31,7 +39,8 @@ public class TocarSom extends JFrame {
         setVisible(true);
     }
 
-    public void play(String nomeAudio) {
+    /*
+    public void tocarMusica(String nomeAudio) {
         // Criando um objeto da classe java.net.URL
         // Chamando a classe principal: Classe.class ou getClass()
         // getClass() não funciona se fizer em um método static
@@ -39,6 +48,40 @@ public class TocarSom extends JFrame {
         // áudio do tipo .wav
         // AudioClip audio = Applet.newAudioClip(url); Depreciados
         // audio.play(); Depreciados
+
+        String caminho = nomeAudio;
+
+        tocarMusicas(caminho);
+    }
+    */
+
+    void tocarMusicas(String musicaLocal) {
+        try {
+            File caminho = new File(musicaLocal); // Guarda o caminho (completo) do arquivo
+            if (caminho.exists()) {
+                AudioInputStream audioInput = AudioSystem.getAudioInputStream(caminho); // Pega o aquivo 
+                Clip clip = AudioSystem.getClip();
+                clip.open(audioInput); // Abre o arquivo
+                clip.start(); // Inicia o arquivo
+                clip.loop(Clip.LOOP_CONTINUOUSLY); // Nunca para, fica em loop
+
+                JOptionPane.showMessageDialog(null, "Presione OK para pausar");
+                long clipTimePosition = clip.getMicrosecondPosition(); // Armazena a exata posição em microsegunodos de onde parou a música
+                clip.stop();
+
+                JOptionPane.showMessageDialog(null, "Clique OK para retomar");
+                clip.setMicrosecondPosition(clipTimePosition); // Configura para a possição que foi guardada
+                clip.start();
+                clip.loop(Clip.LOOP_CONTINUOUSLY);
+
+                JOptionPane.showMessageDialog(null, "Pressione OK para parar de tocar");
+                clip.stop();
+            } else {
+                System.out.println("Não consigo achar o arquivo.");
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
