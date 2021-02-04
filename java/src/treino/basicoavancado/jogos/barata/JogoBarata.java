48cb973727ed194b83fe7b2842d8b22a3244a224
Mateus Araújo <silva.mateusaraujo98@gmail.com>
2021-02-04 20:10:59 -0300

    Jogo Finalizado
    Com erro na pontuação

--- java/src/treino/basicoavancado/jogos/barata/JogoBarata.java
+++ java/src/treino/basicoavancado/jogos/barata/JogoBarata.java
@@ -1,42 +1,92 @@
 package treino.basicoavancado.jogos.barata;
 
 import javax.swing.*;
-
 import java.awt.Font;
 import java.awt.event.*;
 
 public class JogoBarata extends JFrame {
 
+	// Imagens da barata viva e morta
     ImageIcon iconBarata = new ImageIcon(getClass().getResource("barata.png"));
     ImageIcon iconBarataMorta = new ImageIcon(getClass().getResource("baratamorta.png"));
 
-    JLabel lBarata = new JLabel(iconBarata);
+    // Labels para barata, pontuação e tempo
+    JLabel lBarata = new JLabel("iconBarata");
     JLabel lPontos = new JLabel("Pontos:");
     JLabel lContando = new JLabel("0");
     JLabel lTempo = new JLabel("Tempo:");
     JLabel lContandoTempo = new JLabel("20");
+    
+    // Font para pontuação e tempo
     Font fonte = new Font("Arial", Font.BOLD, 50);
 
+    /* Inteiros para colocar a barata e posições aleatórias e dentro da janela,
+     * assim como a conta dos pontos e a contagem regressiva em segundos */
     int x = (int) (Math.random() * 620);
     int y = (int) (Math.random() * 520);
     int conta = 0;
     int segundos = 20;
+    
+    /* Interruptor para ligar/desligar a aleatoriedade da posição da barata
+     * (logo no início do jogo) */
+    boolean feitoInfinito = false;
 
+    /* Construtor da classe, trás os métodos de construção e edição da janela,
+     * e de jogar/continuar o jogo */
     public JogoBarata() {
         editarJanela();
-        JOptionPane.showMessageDialog(null, "Você terá 20 segundos, OK?");
+        continuar();
+    }
+    
+    // Painel para jogar/continuar jogando, trás o método de matar a barata
+    public void continuar() {
+    	JOptionPane.showMessageDialog(null, "Você terá 20 segundos, OK?");
+        feitoInfinito = false;
         Tempo thread = new Tempo();
         thread.start();
         matarBarata();
     }
+    
+    // Painel para escolher se quer jogar novamente ou quer sair do jogo
+    public void escolha() {
+    	int op = JOptionPane.showConfirmDialog(null, "Deseja continuar jogando?");
+		if (op == 0) {
+			conta = 0;
+			lContando.setText(conta + "");
+			segundos = 20;
+			lContandoTempo.setText(segundos + "");
+			continuar();
+		} else if (op == 1) {
+			System.exit(0);
+		} else {
+			System.exit(0);
+		}
+    }
 
+    /* Esta classe faz com que passe o tempo de 20seg simultaneamente,
+     * também mostra a pontuação e desvia para o método escolha() quando acaba o jogo */
     public class Tempo extends Thread {
         boolean feito = false;
         public void run() {
             while (true) {
                 if (segundos == 0) {
-                    feito = true;
-                    JOptionPane.showMessageDialog(null, "Você fez " + conta + " pontos.");
+                	feito = true;
+                	switch (conta) {
+                	case 0:
+                		JOptionPane.showMessageDialog(null, "Treine mais!");
+                		feitoInfinito = true;
+                		escolha();
+                		break;
+                	case 1:
+                		JOptionPane.showMessageDialog(null, "Você fez 1 ponto.");
+                		feitoInfinito = true;
+                		escolha();
+                		break;
+                	default:
+                		JOptionPane.showMessageDialog(null, "Parabéns, você fez " + conta + " pontos.");
+                		feitoInfinito = true;
+                		escolha();
+                	}
                     return;
                 } else {
                     try {Thread.sleep(1000);} catch (Exception erro) {} ;
@@ -47,12 +97,14 @@ public class JogoBarata extends JFrame {
         }
     }
 
+    /* Método de matar a barata com o click do mouse, também faz a barata mudar
+     * de lugar, a medida que o mouse entra no campo do componente/barata */
     public void matarBarata() {
-
-        lBarata.addMouseListener(new MouseListener() {
+    	lBarata.addMouseListener(new MouseListener() {
             @Override
             public void mouseClicked(MouseEvent e) {
-                lContando.setText(++conta + "");
+            	++conta;
+                lContando.setText(conta + "");
                 lBarata.setIcon(iconBarataMorta);
             }
 
@@ -74,15 +126,22 @@ public class JogoBarata extends JFrame {
             public void mouseExited(MouseEvent e) {}
         });
         
+    	/* Esse looping faz com que a barata fique trocando de lugar aleatoriamente
+    	 * e infinitamente assim que começa, a não ser que seja feitoInfinito == true */
         while (true) {
-            try { Thread.sleep(500); } catch (Exception erro) {}
-            lBarata.setIcon(iconBarata);
-            x = (int) (Math.random() * 620);
-            y = (int) (Math.random() * 520);
-            lBarata.setBounds(x, y, 321, 304);
+        	if (feitoInfinito) {
+        		return;
+        	} else {
+        		try { Thread.sleep(1000); } catch (Exception erro) {}
+        		lBarata.setIcon(iconBarata);
+        		x = (int) (Math.random() * 620);
+        		y = (int) (Math.random() * 520);
+        		lBarata.setBounds(x, y, 321, 304);
+        	}
         }
     }
 
+    // Método para criar a janela, adicionar os componentes, setar o layout e as fontes
     public void editarJanela() {
         setLayout(null);
 
@@ -106,6 +165,7 @@ public class JogoBarata extends JFrame {
         setVisible(true);
     }
     
+    // Método Principal - Chama o construtor da própria classe
     public static void main(String[] args) {
         new JogoBarata();
     }
